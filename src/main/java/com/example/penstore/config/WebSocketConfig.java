d6535207d7d9c253873a56d3b9869ac193be1185
GQL <15085883+gql182@user.noreply.gitee.com>
2025-03-14 21:25:00 +0800

    customerService

--- src/main/java/com/example/penstore/config/WebSocketConfig.java
+++ src/main/java/com/example/penstore/config/WebSocketConfig.java
@@ -1,2 +1,16 @@
-package com.example.penstore.config;public class WebSocketConfig {
+package com.example.penstore.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfig {
+    //WebSocket所必须的config
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
 }
--- src/main/java/com/example/penstore/controller/CustomerServiceController.java
+++ src/main/java/com/example/penstore/controller/CustomerServiceController.java
@@ -1,2 +1,15 @@
-package com.example.penstore.controller;public class CustomerServiceController {
+package com.example.penstore.controller;
+
+import com.example.penstore.constants.Pages;
+import com.example.penstore.constants.PathConstants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerServiceController {
+    //以此来实现用户客服网页的跳转
+    @GetMapping(PathConstants.CustomerService)
+    public String customerServicePage() {
+        return Pages.CustomerService;
+    }
 }
